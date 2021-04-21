ba135122e0ba425e251b88a2c30dd9849d994a03
ufochuxian <chen853409335@gmail.com>
2021-04-21 23:00:32 +0800

    flutter demo

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+import 'dart:developer' as developer;
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    return new MaterialApp(
+      title: 'Welcome to Flutter',
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Welcome to Flutter'),
+        ),
+        body: new Center(
+          //child: new Text(wordPair.asPascalCase),
+          child: new RandomWords(),
+        ),
+      ),
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new RandomWordsState();
+  }
+}
+
+class RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    var wordPair = new WordPair.random();
+    developer.log('wordPair:${wordPair}',name : "RandomWordsState");
+    return new Text(wordPair.asPascalCase);
+  }
+}
